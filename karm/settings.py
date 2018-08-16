6695938830cd4a305eb119945b9b6e51eef707e0
Bikram Bharti <bikram.bharti99@gmail.com>
2018-08-16 06:27:27 +0530

    env variables taken in the beginning

--- karm/settings.py
+++ karm/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+PROJ4_LIBRARY_PATH = os.getenv('PROJ4_LIBRARY_PATH')
+print(GDAL_LIBRARY_PATH,GEOS_LIBRARY_PATH)
 
 # Application definition
 
@@ -93,10 +97,7 @@ db_from_env = dj_database_url.config(conn_max_age=500, default='postgis://localh
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
-PROJ4_LIBRARY_PATH = os.getenv('PROJ4_LIBRARY_PATH')
-print(GDAL_LIBRARY_PATH,GEOS_LIBRARY_PATH)
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
