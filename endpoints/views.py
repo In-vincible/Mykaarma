9344cdcf51b6ca7f3deac8dc6b645da058f65f3e
Bikram Bharti <bikram.bharti99@gmail.com>
2018-08-23 17:04:13 +0530

    send_email function added

--- endpoints/views.py
+++ endpoints/views.py
@@ -224,4 +224,12 @@ def sign_out(request):
     logout(request)
     return redirect('/')
 
+def send_email(recipient, subject, body):
 
+    return requests.post(
+        "https://api.mailgun.net/v3/mg.technex.in/messages",
+        auth=("api", "key-cf7f06e72c36031b0097128c90ee896a"),
+        data={"from": "Technex<tech@technex.in>",
+              "to": recipient,
+              "subject": subject,
+              "text": body})
