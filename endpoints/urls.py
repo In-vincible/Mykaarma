0e1bf2f04f141fc4a178d790e33268f0b5916ef3
Bikram Bharti <bikram.bharti99@gmail.com>
2018-08-27 23:45:43 +0530

    urls for index fixed

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -8,12 +8,12 @@ app_name='endpoints'
 
 urlpatterns = [
 	url(r'^location/$', get_dealer, name= 'location'),
-	url(r'^login/', sign_in, name='signIn'),
-	url(r'^signup/', sign_up, name='signup'),
+	url(r'^login/$', sign_in, name='signIn'),
+	url(r'^signup/$', sign_up, name='signup'),
 	url(r'^passChange/', reset_pass, name='password reset'),
 	url(r'^logout/', sign_out, name = 'logout'),
 	url(r'^mail/', send_email, name = 'mail'),
 	url(r'^search/$', search_car, name='search'),
 	url(r'^history/$', past_searches, name='history'),
-	url(r'^', index, name='index')
+	url(r'^$', index, name='index')
 ]
